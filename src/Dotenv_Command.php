1703a655c6a3ea8c48676bf8ac5cf529de8b29bc
Evan Mattson <me@aaemnnost.tv>
2015-08-03 19:50:02 -0400

    list fix

--- src/Dotenv_Command.php
+++ src/Dotenv_Command.php
@@ -212,8 +212,8 @@ class Dotenv_Command extends WP_CLI_Command
     public function _list( $_, $assoc_args )
     {
         $dotenv = get_dotenv_for_read_or_fail( $assoc_args );
-        $keys   = \WP_CLI\Utils\get_flag_value( $assoc_args, 'keys', '' );
-        $keys   = explode( ',', $keys );
+        $keys   = \WP_CLI\Utils\get_flag_value( $assoc_args, 'keys' );
+        $keys   = is_string( $keys ) ? explode( ',', $keys ) : $keys;
         $items  = [ ];
 
         foreach ( $dotenv->get_pairs() as $key => $value )
