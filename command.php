4b3404149dabe8a6b8f1b5077800dddee768a487
Evan Mattson <me@aaemnnost.tv>
2016-03-22 22:47:18 -0400

    refactor files autoloading to command file only

--- command.php
+++ command.php
@@ -1,6 +1,7 @@
-<?php namespace WP_CLI_Dotenv_Command;
+<?php
 
-if ( ! defined('WP_CLI') ) return;
-
-\WP_CLI::add_command( 'dotenv', __NAMESPACE__ . '\\Dotenv_Command' );
-\WP_CLI::add_command( 'dotenv salts', __NAMESPACE__ . '\\Dotenv_Salts_Command' );
+if (defined('WP_CLI') && class_exists('WP_CLI', false)) {
+    WP_CLI::add_command('dotenv', WP_CLI_Dotenv_Command\Dotenv_Command::class);
+    WP_CLI::add_command('dotenv salts', WP_CLI_Dotenv_Command\Dotenv_Salts_Command::class);
+    require_once(__DIR__ . '/src/functions.php');
+}
